3c6c9d928064ba21c6ba97390de9538dc44e0f89
Evan Kirsch <kirsch.j.evan@gmail.com>
2026-06-08 17:08:28 -0500

    Inital commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# :cow: LL - Farming Simulator 25 Lease Livestock
+WIP
+
+## :hammer_and_wrench: Manual Build Instructions
+`git archive -o FS25_LeaseLivestock.zip HEAD`
---
+++ src/LL_leaseLivestock.lua
@@ -0,0 +1,8 @@
+-- LL_leaseLivestock
+-- Main driver class for LL
+--
+
+LL_leaseLivestock = {}
+LL_leaseLivestock.commands = {}
+
+addModEventListener(LL_leaseLivestock)
