1a954c1958aab1b78672a7a7b71471eb8f5f2227
Evan Kirsch <kirsch.j.evan@gmail.com>
2026-06-10 10:15:45 -0500

    Simplify lease message

--- src/gui/LL_AnimalScreenDealer.lua
+++ src/gui/LL_AnimalScreenDealer.lua
@@ -9,18 +9,6 @@ function LL_AnimalScreenDealer:applyLease(animalTypeIndex, itemIndex, numItems)
     local item = self.sourceItems[animalTypeIndex][itemIndex]
     local subTypeIndex = item:getSubTypeIndex()
     local age = item:getAge()
-    local buyPrice = math.abs(item:getPrice()) * numItems
-    local leaseRatePerPeriod = math.ceil(buyPrice / 24)
-
-    local errorCode = LL_AnimalLeaseEvent.validate(
-        self.husbandry, subTypeIndex, age, numItems, leaseRatePerPeriod,
-        self.husbandry:getOwnerFarmId()
-    )
-    if errorCode ~= nil then
-        local mapping = LL_AnimalScreen.LEASE_ERROR_CODE_MAPPING[errorCode]
-        self.errorCallback(mapping ~= nil and g_i18n:getText(mapping.text) or "Lease failed")
-        return false
-    end
 
     self.actionTypeCallback(AnimalScreenBase.ACTION_TYPE_SOURCE, g_i18n:getText("ll_leasingAnimals"))
     g_messageCenter:subscribe(LL_AnimalLeaseEvent, self.onAnimalLeased, self)
@@ -44,11 +32,10 @@ function LL_AnimalScreenDealer:getApplyLeaseConfirmationText(animalTypeIndex, it
     local item = self.sourceItems[animalTypeIndex][itemIndex]
     local buyPrice = math.abs(item:getPrice()) * numItems
     local leaseRate = math.ceil(buyPrice / 24)
-    local textKey = numItems == 1 and "ll_leaseConfirmSingular" or "ll_leaseConfirm"
     local rateStr = g_i18n:formatMoney(leaseRate, 0, true, true)
     local buyoutStr = g_i18n:formatMoney(buyPrice, 0, true, true)
     local animalType = item:getTitle() .. ", " .. item:getName()
-    return string.namedFormat(g_i18n:getText(textKey),
+    return string.namedFormat(g_i18n:getText("ll_leaseConfirm"),
         "numAnimals", numItems,
         "animalType", animalType,
         "rate",       rateStr,
