d7368f005af3fa0c500a07d18e73009405c313b8
Evan Kirsch <kirsch.j.evan@gmail.com>
2026-06-11 15:47:11 -0500

    Add animalcluster patches

--- src/LL_leaseLivestock.lua
+++ src/LL_leaseLivestock.lua
@@ -4,15 +4,9 @@
 
 LL_leaseLivestock = {}
 LL_leaseLivestock.dir = g_currentModDirectory
-LL_leaseLivestock.leases = {}     -- [leaseId] = { farmId, subTypeIndex, numAnimals, leaseRatePerPeriod, buyoutPrice, totalPaid }
-LL_leaseLivestock.nextLeaseId = 1
 
 function LL_leaseLivestock:loadMap()
     g_messageCenter:subscribe(MessageType.PERIOD_CHANGED, self.onPeriodChanged, self)
-    -- TODO: load leases from savegame XML
-
-    -- g_animalScreen.onGuiSetupFinished already fired before mod extraSourceFiles load,
-    -- so we create the Lease button here while g_animalScreen is live.
     if g_animalScreen ~= nil and g_animalScreen.buttonBuy ~= nil then
         g_animalScreen.buttonLease = g_animalScreen.buttonBuy:clone(g_animalScreen.buttonsPanel)
         g_animalScreen.buttonLease:setText(g_i18n:getText("ll_leaseButton"))
@@ -25,43 +19,30 @@ function LL_leaseLivestock:loadMap()
 end
 
 function LL_leaseLivestock:onPeriodChanged()
-    if not g_currentMission:getIsServer() then return end
-    for _, lease in pairs(LL_leaseLivestock.leases) do
-        g_currentMission:addMoney(-lease.leaseRatePerPeriod, lease.farmId, MoneyType.OTHER, true, true)
-        lease.totalPaid = lease.totalPaid + lease.leaseRatePerPeriod
-    end
+    -- TODO: implement
 end
 
 -- Adds animals to the husbandry, charges the first period, and records the lease.
--- Returns the new leaseId.
 function LL_leaseLivestock:addLease(object, subTypeIndex, age, numAnimals, farmId, leaseRatePerPeriod, buyoutPrice)
-    object:addAnimals(subTypeIndex, numAnimals, age)
-    g_currentMission:addMoney(-leaseRatePerPeriod, farmId, MoneyType.OTHER, true, true)
-
-    local leaseId = LL_leaseLivestock.nextLeaseId
-    LL_leaseLivestock.nextLeaseId = LL_leaseLivestock.nextLeaseId + 1
-    LL_leaseLivestock.leases[leaseId] = {
-        farmId             = farmId,
-        subTypeIndex       = subTypeIndex,
-        numAnimals         = numAnimals,
-        leaseRatePerPeriod = leaseRatePerPeriod,
-        buyoutPrice        = buyoutPrice,
-        totalPaid          = leaseRatePerPeriod,
-    }
-    -- TODO: persist leases to savegame XML
-    return leaseId
-end
-
--- Charges the remaining buyout balance and clears the lease record.
--- Animals stay in the barn — they are now fully owned.
-function LL_leaseLivestock:rebuyLease(leaseId, farmId)
-    local lease     = LL_leaseLivestock.leases[leaseId]
-    local remaining = math.max(lease.buyoutPrice - lease.totalPaid, 0)
-    if remaining > 0 then
-        g_currentMission:addMoney(-remaining, farmId, MoneyType.NEW_ANIMALS_COST, true, true)
+    local animalSystem = g_currentMission.animalSystem
+    local cluster = animalSystem:createClusterFromSubTypeIndex(subTypeIndex)
+    cluster.isLeased = true
+    if cluster:getSupportsMerging() then
+        cluster.numAnimals = numAnimals
+        cluster.age = age
+        cluster.subTypeIndex = subTypeIndex
+        object:addCluster(cluster)
+    else
+        for _ = 1, numAnimals do
+            cluster = animalSystem:createClusterFromSubTypeIndex(subTypeIndex)
+            cluster.isLeased = true
+            cluster.numAnimals = 1
+            cluster.age = age
+            object:addCluster(cluster)
+        end
     end
-    LL_leaseLivestock.leases[leaseId] = nil
-    -- TODO: persist leases to savegame XML
+
+    g_currentMission:addMoney(-leaseRatePerPeriod, farmId, MoneyType.OTHER, true, true)
 end
 
 addModEventListener(LL_leaseLivestock)
---
+++ src/cluster/LL_AnimalClusterLeased.lua
@@ -0,0 +1,65 @@
+-- LL_AnimalClusterLeased
+-- Patches AnimalCluster to isolate leased animals from owned ones.
+--
+
+AnimalCluster.getHash = Utils.overwrittenFunction(AnimalCluster.getHash,
+    function(self, superFunc)
+        local hash = superFunc(self)
+        if self.isLeased then
+            hash = hash + 1000000000000  -- prevent overlap
+        end
+        return hash
+    end
+)
+
+-- Prevent merges between leased and non-leased clusters.
+AnimalCluster.merge = Utils.overwrittenFunction(AnimalCluster.merge,
+    function(self, superFunc, otherCluster)
+        if (self.isLeased or false) ~= (otherCluster.isLeased or false) then
+            return false
+        end
+        return superFunc(self, otherCluster)
+    end
+)
+
+-- Sync isLeased over the network.
+AnimalCluster.readStream = Utils.overwrittenFunction(AnimalCluster.readStream,
+    function(self, superFunc, streamId, connection)
+        superFunc(self, streamId, connection)
+        self.isLeased = streamReadBool(streamId)
+    end
+)
+
+AnimalCluster.writeStream = Utils.overwrittenFunction(AnimalCluster.writeStream,
+    function(self, superFunc, streamId, connection)
+        superFunc(self, streamId, connection)
+        streamWriteBool(streamId, self.isLeased or false)
+    end
+)
+
+-- Register #isLeased in the savegame schema.
+AnimalCluster.registerSavegameXMLPaths = Utils.overwrittenFunction(
+    AnimalCluster.registerSavegameXMLPaths,
+    function(schema, superFunc, basePath)
+        superFunc(schema, basePath)
+        schema:register(XMLValueType.BOOL, basePath .. "#isLeased", false)
+    end
+)
+
+-- Persist isLeased to savegame XML.
+AnimalCluster.saveToXMLFile = Utils.overwrittenFunction(AnimalCluster.saveToXMLFile,
+    function(self, superFunc, xmlFile, key, usedModNames)
+        superFunc(self, xmlFile, key, usedModNames)
+        if self.isLeased then
+            xmlFile:setBool(key .. "#isLeased", true)
+        end
+    end
+)
+
+AnimalCluster.loadFromXMLFile = Utils.overwrittenFunction(AnimalCluster.loadFromXMLFile,
+    function(self, superFunc, xmlFile, key)
+        local result = superFunc(self, xmlFile, key)
+        self.isLeased = xmlFile:getBool(key .. "#isLeased", false)
+        return result
+    end
+)
